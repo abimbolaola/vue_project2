1400d4d435e7c218b81be612863c2f5899626460
Abimbola Oluwatobiloba J <71207758+JCWORLD-SOFTWARE@users.noreply.github.com>
2022-09-25 02:08:18 +0100

    ImageList

---
+++ src/components/ImageList/ImageList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-if="isLoggedIn">
+        <div class="image-container">
+            <img v-for="image in allImages" :key="image.id" :src="image.link" />
+        </div>
+    </div>
+
+    <h2 v-else>Please login to continue</h2>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+    name: 'ImageList',
+    computed: mapGetters(['allImages', 'isLoggedIn']),
+    methods: mapActions(['fetchImages']),
+    created() {
+        this.fetchImages();
+    },
+};
+</script>
+
+<style scoped>
+.image-container {
+    column-count: 3;
+    column-gap: 0;
+}
+
+img {
+    max-width: 100%;
+    padding: 5px;
+}
+</style>
